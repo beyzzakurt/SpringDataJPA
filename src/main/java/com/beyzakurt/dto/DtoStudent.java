9fd678d72d20a7df17c2556a0396ae577045cb0b
beyzzakurt <beyzzakurt34@gmail.com>
2025-04-25 15:32:53 +0300

    Create DtoStudent object

---
+++ src/main/java/com/beyzakurt/dto/DtoStudent.java
@@ -0,0 +1,14 @@
+package com.beyzakurt.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class DtoStudent {
+
+    private String firstName;
+    private String lastName;
+}
